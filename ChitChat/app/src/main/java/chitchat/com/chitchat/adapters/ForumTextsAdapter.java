a022e4674e708093bc5ab02f1a6ab7c3a7b13458
Brian Lusina <lusinabrian@gmail.com>
2016-09-07 18:00:26 +0300

    Add Forum Texts Adapter

---
+++ ChitChat/app/src/main/java/chitchat/com/chitchat/adapters/ForumTextsAdapter.java
@@ -0,0 +1,115 @@
+package chitchat.com.chitchat.adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.List;
+
+import chitchat.com.chitchat.models.ForumModel;
+import chitchat.com.chitchat.models.ForumTextsModel;
+
+/**
+ * Project: ChitChat
+ * Package: chitchat.com.chitchat.adapters
+ * Created by lusinabrian on 07/09/16 at 17:47
+ * <p>
+ * Description:
+ */
+
+public class ForumTextsAdapter extends RecyclerView.Adapter<ForumTextsAdapter.ViewHolder> {
+    private Context mContext;
+    private List<ForumTextsModel> forumTextsModelList;
+    private int itemLayout;
+
+    /**Constuctor to initialize this adapter
+     * @param mContext Context in which this adapter is called
+     * @param forumModelList List in which data will be extracted and transfered to items
+     * @param itemLayout Layout to inflate data with**/
+    public ForumTextsAdapter(Context mContext, List<ForumModel> forumModelList, int itemLayout) {
+        this.mContext = mContext;
+        this.forumTextsModelList = forumTextsModelList;
+        itemLayout = itemLayout;
+    }
+
+    /**
+     * Called when RecyclerView needs a new {@link ViewHolder} of the given type to represent
+     * an item.
+     * <p>
+     * This new ViewHolder should be constructed with a new View that can represent the items
+     * of the given type. You can either create a new View manually or inflate it from an XML
+     * layout file.
+     * <p>
+     * The new ViewHolder will be used to display items of the adapter using
+     * {onBindViewHolder(ViewHolder, int, List)}. Since it will be re-used to display
+     * different items in the data set, it is a good idea to cache references to sub views of
+     * the View to avoid unnecessary {View#findViewById(int)} calls.
+     *
+     * @param parent   The ViewGroup into which the new View will be added after it is bound to
+     *                 an adapter position.
+     * @param viewType The view type of the new View.
+     * @return A new ViewHolder that holds a View of the given view type.
+     * @see #getItemViewType(int)
+     * @see #onBindViewHolder(ViewHolder, int)
+     */
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(itemLayout,parent, false);
+        return new ViewHolder(itemView);
+    }
+
+    /**
+     * Called by RecyclerView to display the data at the specified position. This method should
+     * update the contents of the { ViewHolder#itemView} to reflect the item at the given
+     * position.
+     * <p>
+     * Note that unlike {@link ListView}, RecyclerView will not call this method
+     * again if the position of the item changes in the data set unless the item itself is
+     * invalidated or the new position cannot be determined. For this reason, you should only
+     * use the <code>position</code> parameter while acquiring the related data item inside
+     * this method and should not keep a copy of it. If you need the position of an item later
+     * on (e.g. in a click listener), use { ViewHolder#getAdapterPosition()} which will
+     * have the updated adapter position.
+     * <p>
+     * Override {#onBindViewHolder(ViewHolder, int, List)} instead if Adapter can
+     * handle efficient partial bind.
+     *
+     * @param holder   The ViewHolder which should be updated to represent the contents of the
+     *                 item at the given position in the data set.
+     * @param position The position of the item within the adapter's data set.
+     */
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        ForumTextsModel forumTextsModel = forumTextsModelList.get(position);
+        holder.itemView.setTag(forumTextsModel);
+        holder.bind(forumTextsModel);
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+        private ImageView userImage;
+        private TextView userName, messageTime,message;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+
+        public void bind(ForumTextsModel forumTextsModel){
+
+        }
+    }
+
+    /**
+     * Returns the total number of items in the data set held by the adapter.
+     * @return The total number of items in this adapter.
+     */
+    @Override
+    public int getItemCount() {
+        return forumTextsModelList.size();
+    }
+
+}
