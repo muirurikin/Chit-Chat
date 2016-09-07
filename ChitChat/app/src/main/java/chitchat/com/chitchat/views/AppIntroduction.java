15685b007aa7bff194626bbfef9643f900081d88
Brian Lusina <lusinabrian@gmail.com>
2016-09-07 17:10:33 +0300

    Add app introduction

--- ChitChat/app/src/main/java/chitchat/com/chitchat/views/AppIntroduction.java
+++ ChitChat/app/src/main/java/chitchat/com/chitchat/views/AppIntroduction.java
@@ -1,11 +1,17 @@
 package chitchat.com.chitchat.views;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
 
 import com.github.paolorotolo.appintro.AppIntro2;
 import com.github.paolorotolo.appintro.AppIntroFragment;
+
+import chitchat.com.chitchat.MainActivity;
+import chitchat.com.chitchat.R;
 
 /**
  * Project: ChitChat
@@ -21,6 +27,43 @@ public class AppIntroduction extends AppIntro2{
     public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
 
-        addSlide(AppIntroFragment.newInstance("","",, ContextCompat.getColor()));
+        //add fragments for the introduction (3 slides)
+        //TODO: change titles and descriptions
+        addSlide(AppIntroFragment.newInstance("","",R.mipmap.ic_launcher, ContextCompat.getColor(this,R.color.dark_orange)));
+        addSlide(AppIntroFragment.newInstance("","",R.mipmap.ic_launcher, ContextCompat.getColor(this,R.color.orange_1)));
+        addSlide(AppIntroFragment.newInstance("","",R.mipmap.ic_launcher, ContextCompat.getColor(this,R.color.light_orange2)));
+
+        /*animation*/
+        setDepthAnimation();
+    }
+
+    @Override
+    public void onSlideChanged(@Nullable Fragment oldFragment, @Nullable Fragment newFragment) {
+        super.onSlideChanged(oldFragment, newFragment);
+        //do nothing
+    }
+
+    @Override
+    public void onDonePressed(Fragment currentFragment) {
+        super.onDonePressed(currentFragment);
+        //open main activity
+        openMain();
+        //kill this to save memory
+        finish();
+    }
+
+    @Override
+    public void onSkipPressed(Fragment currentFragment) {
+        super.onSkipPressed(currentFragment);
+        //open Main activity
+        openMain();
+        finish();
+    }
+
+
+    /**open main activity*/
+    public void openMain(){
+        Intent openMain = new Intent(AppIntroduction.this, MainActivity.class);
+        startActivity(openMain);
     }
 }
