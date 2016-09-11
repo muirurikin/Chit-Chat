26f9d4d6337ab33163444f4f8dd0e5fd435cab8d
Brian Lusina <lusinabrian@gmail.com>
2016-09-11 15:10:57 +0300

    Remove ChitChat folder

---
+++ app/src/main/java/chitchat/com/chitchat/views/LoginActivity.java
@@ -0,0 +1,14 @@
+package chitchat.com.chitchat.views;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Project: Chit-Chat
+ * Package: chitchat.com.chitchat.views
+ * Created by lusinabrian on 11/09/16 at 15:07
+ * <p>
+ * Description:
+ */
+
+public class LoginActivity extends AppCompatActivity {
+}
