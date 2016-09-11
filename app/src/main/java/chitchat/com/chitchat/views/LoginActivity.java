5130128dfb4e37a469d936d549da64402b98013a
Brian Lusina <lusinabrian@gmail.com>
2016-09-11 15:12:21 +0300

    Add login layout

--- app/src/main/java/chitchat/com/chitchat/views/LoginActivity.java
+++ app/src/main/java/chitchat/com/chitchat/views/LoginActivity.java
@@ -1,14 +1,24 @@
 package chitchat.com.chitchat.views;
 
+import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
+
+import chitchat.com.chitchat.R;
 
 /**
  * Project: Chit-Chat
  * Package: chitchat.com.chitchat.views
  * Created by lusinabrian on 11/09/16 at 15:07
  * <p>
- * Description:
+ * Description: LoginActivity to authenticate User
  */
 
 public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.loginactivity_layout);
+    }
 }
