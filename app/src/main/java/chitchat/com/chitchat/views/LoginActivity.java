e11d1233ef19c707967dd8f4a0634b2d4c4b5d19
Brian Lusina <lusinabrian@gmail.com>
2016-09-11 15:56:06 +0300

    Add Twitter Callback

--- app/src/main/java/chitchat/com/chitchat/views/LoginActivity.java
+++ app/src/main/java/chitchat/com/chitchat/views/LoginActivity.java
@@ -6,11 +6,17 @@ import android.os.PersistableBundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
 
 import chitchat.com.chitchat.R;
 import chitchat.com.chitchat.presenter.Contract;
@@ -28,6 +34,7 @@ public class LoginActivity extends AppCompatActivity {
     private static final String LOGINACTIVITY = LoginActivity.class.getSimpleName();
     private FirebaseAuth firebaseAuth;
     private FirebaseAuth.AuthStateListener authStateListener;
+    private TwitterLoginButton twitterLoginButton;
 
     @Override
     public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
@@ -36,7 +43,8 @@ public class LoginActivity extends AppCompatActivity {
         Fabric.with(this, new Twitter(authConfig));
 
         setContentView(R.layout.loginactivity_layout);
-
+        initViews();
+        initializeLogins();
         // Initialize Firebase Auth
         firebaseAuth = FirebaseAuth.getInstance();
 
@@ -55,7 +63,45 @@ public class LoginActivity extends AppCompatActivity {
                     Log.d(LOGINACTIVITY, "AuthStateChanged:signedOut:");
                 }
             }
-        }
+        };
     }
 
+
+    /**Initialize UI Controls*/
+    private void initViews() {
+        twitterLoginButton = (TwitterLoginButton) findViewById(R.id.twitter_login_button);
+    }
+
+    /**Initialize logins by TwitterLogin, FacebookLogin and GoogleLogin*/
+    private void initializeLogins() {
+        /*handle Twitter callback*/
+        twitterLoginButton.setCallback(new Callback<TwitterSession>() {
+            @Override
+            public void success(Result<TwitterSession> result) {
+
+            }
+
+            @Override
+            public void failure(TwitterException exception) {
+                Log.w(LOGINACTIVITY, "twitterLogin:failure", exception);
+                //TODO: change display of login failure
+                Toast.makeText(LoginActivity.this, "Twitter Login failure", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        firebaseAuth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (authStateListener != null) {
+            firebaseAuth.removeAuthStateListener(authStateListener);
+        }
+    }
 }
