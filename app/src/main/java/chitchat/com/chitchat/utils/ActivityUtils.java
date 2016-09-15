289f3c85d22fc375da6519eec6f166b0ac428499
Brian Lusina <lusinabrian@gmail.com>
2016-09-15 14:01:53 +0300

    Remove checkNotNull

--- app/src/main/java/chitchat/com/chitchat/utils/ActivityUtils.java
+++ app/src/main/java/chitchat/com/chitchat/utils/ActivityUtils.java
@@ -17,12 +17,13 @@ import static com.google.common.base.Preconditions.checkNotNull;
 public class ActivityUtils {
     /**
      * The {@code fragment} is added to the container view with id {@code frameId}. The operation is
-     * performed by the {@code fragmentManager}.
-     */
+     * performed by the {@code fragmentManager}.*/
     public static void addFragmentToActivity (@NonNull FragmentManager fragmentManager,
                                               @NonNull Fragment fragment, int frameId) {
+/*
         checkNotNull(fragmentManager);
         checkNotNull(fragment);
+*/
         FragmentTransaction transaction = fragmentManager.beginTransaction();
         transaction.add(frameId, fragment);
         transaction.commit();
