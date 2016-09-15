c1cd2627b7a1e883dc37aa7f4b2a69cfaaa4af18
Brian Lusina <lusinabrian@gmail.com>
2016-09-15 13:00:09 +0300

    Add BaseView interface

---
+++ app/src/main/java/chitchat/com/chitchat/BaseView.java
@@ -0,0 +1,11 @@
+package chitchat.com.chitchat;
+
+/**
+ * Project: Chit-Chat
+ * Package: chitchat.com.chitchat
+ * Created by lusinabrian on 15/09/16 at 12:59
+ */
+
+public interface BaseView<T> {
+    void setPresenter(T presenter);
+}
